595238230675105d5d9a1a116e94da129dff2000
Manuel Pissanos <mpissano@gmail.com>
2019-09-11 12:24:14 -0400

    review

--- src/components/ServiceArea.jsx
+++ src/components/ServiceArea.jsx
@@ -26,3 +26,31 @@ class ServiceArea extends Component {
 }
 
 export default ServiceArea;
+
+
+// class ServiceArea extends Component {
+//   render() {
+//     return (
+//       <div className="service-area">
+//         <h3>SERVICE AREA</h3>
+//         <div className="line"></div>
+//         <div className="service-areas">
+//           <div className="service-areas service-areas-left">
+//             <div> Dania Beach</div>
+//             <div>Davie</div>
+//             <div>Plantation</div>
+//             <div>Hollywood</div>
+//           </div>
+//           <div className="service-areas service-areas-left">
+//             <div>Coral Springs</div>
+//             <div>Pompano</div>
+//             <div>Pembroke Pines</div>
+//             <div>Dania Beach</div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ServiceArea;
